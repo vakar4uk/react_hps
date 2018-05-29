9f028277bf6c3310dd62e49b7ded690d4dee58ed
vakar4uk <31296706+vakar4uk@users.noreply.github.com>
2018-05-29 14:38:57 -0500

    BurgerBuilder v.2
    Some Dynamic Functionality added

--- src/playground/components/Burger/Burger.js
+++ src/playground/components/Burger/Burger.js
@@ -4,12 +4,19 @@ import classes from './Burger.css';
 import BurgerIngridient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transfromedIngredients = Object.keys(props.ingredients)
+    let transfromedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngridient key={igKey + i} type={igKey} />;
             });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+        if(transfromedIngredients.length === 0) {          
+            transfromedIngredients = <p>Please start adding ingridients!</p>;            
+        }       
+        
     return (
         <div className={classes.Burger}>
             <BurgerIngridient type="bread-top" />
