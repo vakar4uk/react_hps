8c0e349be7fba011a038ef8f05363ebbd4618d1b
vakar4uk <31296706+vakar4uk@users.noreply.github.com>
2018-05-31 13:47:28 -0500

    BurgerBuilder v.4
    Basic functionality/styles applied

--- src/playground/components/Burger/OrderSummary/OrderSummary.js
+++ src/playground/components/Burger/OrderSummary/OrderSummary.js
@@ -1,27 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../../hoc/Aux';
 
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
-            return <li key={igKey}><span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}</li>
+            return <li key={igKey}><span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}</li>
         });
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients: </p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total price: ${props.price.toFixed(2)}</strong></p>
-            <p>Continue to checkout?</p>
-            <Button btnType='Danger' clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType='Success' clicked={props.purchaseContinued}>SUCCESS</Button>
-        </Aux>
-    );
-};
 
-export default orderSummary;
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients: </p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total price: ${this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to checkout?</p>
+                <Button btnType='Danger' clicked={this.props.purchaseCanceled}>CANCEL</Button>
+                <Button btnType='Success' clicked={this.props.purchaseContinued}>SUCCESS</Button>
+            </Aux>
+        );
+    }
+}
+
+
+export default OrderSummary;
--- src/playground/components/Navigation/Toolbar/Toolbar.js
+++ src/playground/components/Navigation/Toolbar/Toolbar.js
@@ -4,14 +4,17 @@ import classes from './Toolbar.css';
 
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => ( 
     <header className={classes.Toolbar}>
-        <div>MENU</div>
+        <DrawerToggle clicked={props.drawerToggleClicked} />
         <div className={classes.Logo}>
             <Logo />
         </div>
-        <NavigationItems />
+        <nav className={classes.DesktopOnly}>
+            <NavigationItems />
+        </nav>
         
     </header>
 ); 
